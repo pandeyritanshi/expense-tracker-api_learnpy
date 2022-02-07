dd5497f15ae0ba8d12c16523c21321e74952741c
pandeyritanshi <pandeyrianshi7865@gmail.com>
2022-02-07 16:58:43 +0530

    Added the first test

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+# from django.test import TestCase
+from unittest import TestCase
 
 # Create your tests here.
+def sum_int(a, b):
+    return a + b
+
+
+class TestSum(TestCase):
+    def test_sum(self):
+        self.assertEqual(sum_int(1, 2), 3)
